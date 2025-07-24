c9ca95460eb6a9729e50cf7232841c69c05a3dfb
eabalestra <agusbalestra0@gmail.com>
2025-07-24 14:09:06 -0300

    Add Logger class for logging functionality

---
+++ src/logger/logger.py
@@ -0,0 +1,22 @@
+import logging
+
+
+class Logger:
+    def __init__(self, output_path) -> None:
+        logging.basicConfig(filename=output_path, level=logging.DEBUG)
+        self.logger = logging.getLogger(__name__)
+
+    def log(self, message: str) -> None:
+        self.logger.info(message)
+
+    def log_error(self, message: str) -> None:
+        self.logger.error(message)
+
+    def log_warning(self, message: str) -> None:
+        self.logger.warning(message)
+
+    def log_debug(self, message: str) -> None:
+        self.logger.debug(message)
+
+    def close(self) -> None:
+        logging.shutdown()
