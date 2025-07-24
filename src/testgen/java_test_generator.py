15fc1a077a5980d07fc37fde8a379dbe63741e24
eabalestra <agusbalestra0@gmail.com>
2025-07-24 14:09:40 -0300

    Add JavaTestGenerator class for generating Java test prompts and executing them

---
+++ src/testgen/java_test_generator.py
@@ -0,0 +1,44 @@
+
+
+from java_compiler.java_compiler import JavaCompiler
+from llmservice.llm_service import LLMService
+
+from prompt.prompt import Prompt
+from prompt.prompt_id import PromptID
+from specs.specs import Specs
+from subject.subject import Subject
+
+
+class JavaTestGenerator:
+
+    def __init__(self, subject: Subject, specs: Specs):
+        self.llm_service = LLMService()
+        # It could potentially be constructed from the subject
+        self.java_compiler = JavaCompiler()
+        self.subject = subject
+        self.specs = specs
+        self.prompts = []
+        self.llm_response = ""
+
+    def generate_prompts(self, prompt_id, class_code, method_code, spec):
+        self.prompts.append(Prompt(prompt_id, class_code, method_code, spec))
+
+    def execute(self, pid, mid):
+        combined_responses = ""
+        for prompt in self.prompts:
+            if prompt.id is not pid:
+                continue
+            response = self.llm_service.execute_prompt(
+                mid, prompt.prompt, prompt.format_instructions)
+            if response is not None:
+                combined_responses += response
+        return combined_responses
+
+    def generate_test(self, class_code, method_code, spec, prompt_ids=PromptID.all(), models_ids=[]):
+        self.llm_response = ""
+        for mid in models_ids:
+            for pid in prompt_ids:
+                self.generate_prompts(pid, class_code, method_code, spec)
+                llm_output = self.execute(pid, mid)
+                self.llm_response += llm_output
+        return self.llm_response
