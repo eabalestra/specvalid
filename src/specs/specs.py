57e4e60409e421d0a376c7bddb591b86315f530b
eabalestra <agusbalestra0@gmail.com>
2025-07-26 20:26:21 -0300

    Implement Specs class with methods for parsing specifications from a file

--- src/specs/specs.py
+++ src/specs/specs.py
@@ -1,3 +1,26 @@
 
+
+from re import search
+
+
 class Specs:
-    raise NotImplementedError("Specs class is not implemented yet.")
+
+    def __init__(self, spec_file: str):
+        self.assertions_file_path = spec_file
+
+    def is_inv_line(self, line: str) -> bool:
+        return not (search(":::OBJECT", line) or
+                    search("==============", line) or
+                    search(":::POSTCONDITION", line) or
+                    search(":::ENTER", line) or
+                    search("buckets=", line) or
+                    search("specs=", line))
+
+    def parse_and_collect_specs(self) -> set:
+        specs = []
+        with open(self.assertions_file_path, "r", encoding="utf-8") as file:
+            lines = file.read().splitlines()
+            for line in lines:
+                if self.is_inv_line(line):
+                    specs.append(line)
+        return set(specs)
