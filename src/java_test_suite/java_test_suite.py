d6d7731b693a729d0826ecd6fea65686cd613f51
eabalestra <agusbalestra0@gmail.com>
2025-07-27 18:06:19 -0300

    Refactor JavaTestSuite class constructor to accept path_to_suite parameter and update assertion removal logic

--- src/java_test_suite/java_test_suite.py
+++ src/java_test_suite/java_test_suite.py
@@ -1,5 +1,16 @@
 
 
+import re
+
+
 class JavaTestSuite:
-    def __init__(self, project_path):
-        self.project_path = project_path
+
+    def __init__(self, path_to_suite: str):
+        self.path_to_suite = path_to_suite
+
+    def remove_assertions_from_test(self, test_code: str) -> str:
+        assertion_regex = re.compile(
+            r'^\s*(?:[a-zA-Z0-9_.]+\.)?(?:assert\w*\b.*?;|fail\w*\b.*?;).*$',
+            re.MULTILINE | re.IGNORECASE
+        )
+        return re.sub(assertion_regex, '', test_code)
