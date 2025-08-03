ce246ff0d7f7865e7a209b20ae0c12b9cad8cdbd
eabalestra <agusbalestra0@gmail.com>
2025-08-03 20:32:18 -0300

    Refactor Subject class to use JavaCodeExtractor for method code extraction

--- src/subject/subject.py
+++ src/subject/subject.py
@@ -1,8 +1,7 @@
-
 import os
 from zipfile import Path
 
-from code_extractor.code_extractor import CodeExtractor
+from java_code_extractor.java_code_extractor import JavaCodeExtractor
 from java_test_driver.java_test_driver import JavaTestDriver
 from java_test_suite.java_test_suite import JavaTestSuite
 from specs.specs import Specs
@@ -19,7 +18,7 @@ class Subject:
         spec_file: str,
         method_name: str,
         java_test_suite: JavaTestSuite,
-        java_test_driver: JavaTestDriver
+        java_test_driver: JavaTestDriver,
     ):
         self.class_code = self._load_class_code(class_path_src)
         self.method_code = self._load_method_code(class_path_src, method_name)
@@ -32,12 +31,11 @@ class Subject:
 
     def _load_class_code(self, class_path_src: str) -> str:
         if not os.path.exists(class_path_src):
-            raise FileNotFoundError(
-                f"Class source file not found: {class_path_src}")
-        with open(class_path_src, 'r') as file:
+            raise FileNotFoundError(f"Class source file not found: {class_path_src}")
+        with open(class_path_src, "r") as file:
             return file.read()
 
     def _load_method_code(self, class_path_src: str, method_name: str) -> str:
-        code_extractor = CodeExtractor()
+        code_extractor = JavaCodeExtractor()
         class_code = self._load_class_code(class_path_src)
         return code_extractor.extract_method_code(class_code, method_name)
