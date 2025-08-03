f2c1b0d60f6dca738b9a1026efb8ef433fbcf8f7
eabalestra <agusbalestra0@gmail.com>
2025-08-03 20:33:23 -0300

    Replace JavaTestPreparer with JavaTestFileUpdater in CLI and remove related comments

--- src/cli.py
+++ src/cli.py
@@ -1,6 +1,6 @@
 from argsparser.parser import build_parser
 from core import run_testgen
-from java_test_preparer.java_test_preparer import JavaTestPreparer
+from java_test_file_updater.java_test_file_updater import JavaTestFileUpdater
 from llmservice.llm_service import LLMService
 from prompt.prompt_id import PromptID
 
@@ -18,17 +18,6 @@ def main():
         return
 
     if args.command == "testgen":
-        # Run test generation using LLM's
         run_testgen(args)
-        # Prepare the augmented test files (old tests + generated tests)
-        new_test_suite = JavaTestPreparer.prepare_test_file(
-            args.test_suite, "Augmented", is_driver=False
-        )
-        new_test_driver = JavaTestPreparer.prepare_test_file(
-            args.test_driver, "Augmented", is_driver=True
-        )
-        # run fix generated test files
-        # run discard uncompilable test files
-        # run append test files to the destination
     elif args.command == "mutgen":
         raise NotImplementedError("Mutgen functionality is not implemented yet.")
