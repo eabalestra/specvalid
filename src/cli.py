5b6b972733fa0b7c99c6ee0dc59502681779e62d
eabalestra <agusbalestra0@gmail.com>
2025-08-03 17:07:15 -0300

    Add JavaTestPreparer class for test file preparation and refactor cli.py

--- src/cli.py
+++ src/cli.py
@@ -1,5 +1,6 @@
 from argsparser.parser import build_parser
 from core import run_testgen
+from java_test_preparer.java_test_preparer import JavaTestPreparer
 from llmservice.llm_service import LLMService
 from prompt.prompt_id import PromptID
 
@@ -10,14 +11,24 @@ def main():
 
     if args.list_llms:
         llm_service = LLMService()
-        print("Supported LLMs:", llm_service.get_all_models(), sep='\n')
+        print("Supported LLMs:", llm_service.get_all_models(), sep="\n")
         return
     if args.list_prompts:
-        print("Available PromptIDs:", *[p.name for p in PromptID], sep='\n')
+        print("Available PromptIDs:", *[p.name for p in PromptID], sep="\n")
         return
 
     if args.command == "testgen":
+        # Run test generation using LLM's
         run_testgen(args)
+        # Prepare the augmented test files (old tests + generated tests)
+        new_test_suite = JavaTestPreparer.prepare_test_file(
+            args.test_suite, "Augmented", is_driver=False
+        )
+        new_test_driver = JavaTestPreparer.prepare_test_file(
+            args.test_driver, "Augmented", is_driver=True
+        )
+        # run fix generated test files
+        # run discard uncompilable test files
+        # run append test files to the destination
     elif args.command == "mutgen":
-        raise NotImplementedError(
-            "Mutgen functionality is not implemented yet.")
+        raise NotImplementedError("Mutgen functionality is not implemented yet.")
--- src/java_test_driver/java_test_driver.py
+++ src/java_test_driver/java_test_driver.py
@@ -1,5 +1,4 @@
 
 class JavaTestDriver:
-    def __init__(self, java_class_path, test_class_name):
-        self.java_class_path = java_class_path
-        self.test_class_name = test_class_name
+    def __init__(self):
+        pass
