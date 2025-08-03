81919c9dd9936d200c9ff32430a713f4eb8009ee
eabalestra <agusbalestra0@gmail.com>
2025-08-03 20:33:58 -0300

    Refactor JavaTestGenService to streamline test generation process and improve code organization

--- src/services/java_tesgen_service.py
+++ src/services/java_tesgen_service.py
@@ -1,6 +1,6 @@
-
 import time
 
+from java_code_extractor.java_code_extractor import JavaCodeExtractor
 from subject.subject import Subject
 from testgen.java_test_generator import JavaTestGenerator
 from logger.logger import Logger
@@ -13,22 +13,23 @@ class JavaTestGenService:
         subject: Subject,
         test_generator: JavaTestGenerator,
         logger: Logger,
-        timestamp_logger: Logger
+        timestamp_logger: Logger,
     ):
         self.subject = subject
         self.test_generator = test_generator
         self.logger = logger
         self.timestamp_logger = timestamp_logger
         self.assertions_from_specfuzzer = self.subject.collect_specs()
+        self.code_extractor = JavaCodeExtractor()
 
     def run(self, prompts: list, models: list):
         self.logger.log(f"Starting test generation for {self.subject}...")
         total_time = 0.0
         for assertion in self.assertions_from_specfuzzer:
-            assertion = self.subject.specs.transform_specification_vars(
-                assertion)
+            assertion = self.subject.specs.transform_specification_vars(assertion)
             self.logger.log(f"Generating test for assertion: {assertion}")
             start_time = time.time()
+
             # Use LLMs to generate tests that invalidate the assertion
             llm_response = self.test_generator.generate_test(
                 class_code=self.subject.class_code,
@@ -40,14 +41,22 @@ class JavaTestGenService:
             elapsed_time = time.time() - start_time
             total_time += elapsed_time
 
-            # Post-process the generated test
-            generated_test = llm_response
+            self.logger.log(f"LLM response: {llm_response}")
 
-            self.logger.log(f"Generated test: \n{generated_test}")
+            test_code = self.process_llm_test_response(llm_response)
+            self.subject.test_suite.add_test(test_code)
+
+            self.logger.log(f"Generated test: \n{test_code}")
             self.timestamp_logger.log(
                 f"Test generation for assertion '{assertion}' took "
-                f"{elapsed_time:.2f} seconds")
+                f"{elapsed_time:.2f} seconds"
+            )
         self.timestamp_logger.log(
-            f"Total test generation time: {total_time:.2f} seconds")
-        self.logger.log(
-            f"Finished test generation for {self.subject}.")
+            f"Total test generation time: {total_time:.2f} seconds"
+        )
+        self.logger.log(f"Finished test generation for {self.subject}.")
+
+    def process_llm_test_response(self, llm_response: str) -> str:
+        test_code = self.code_extractor.extract_test_code_from_response(llm_response)
+        test_code = self.subject.test_suite.remove_assertions_from_test(test_code)
+        return test_code
