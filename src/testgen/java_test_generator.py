266af36b97d992f3bc13b498f0b36a0db7f57153
eabalestra <agusbalestra0@gmail.com>
2025-08-03 17:07:02 -0300

    Refactor code formatting in JavaTestGenerator class for improved readability

--- src/testgen/java_test_generator.py
+++ src/testgen/java_test_generator.py
@@ -1,5 +1,3 @@
-
-
 from java_compiler.java_compiler import JavaCompiler
 from llmservice.llm_service import LLMService
 
@@ -29,15 +27,15 @@ class JavaTestGenerator:
             if prompt.id is not pid:
                 continue
             response = self.llm_service.execute_prompt(
-                mid,
-                prompt.prompt,
-                prompt.format_instructions
+                mid, prompt.prompt, prompt.format_instructions
             )
             if response is not None:
                 combined_responses += response
         return combined_responses
 
-    def generate_test(self, class_code, method_code, spec, prompt_ids=PromptID.all(), models_ids=[]):
+    def generate_test(
+        self, class_code, method_code, spec, prompt_ids=PromptID.all(), models_ids=[]
+    ):
         self.llm_response = ""
         for mid in models_ids:
             for pid in prompt_ids:
