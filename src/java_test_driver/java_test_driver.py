8ef83e22036df2d0e7276232e035c8cc6f76a034
eabalestra <agusbalestra0@gmail.com>
2025-08-07 15:03:02 -0300

    Refactor JavaTestDriver to initialize with test_driver_path and retrieve package name using Utils

--- src/java_test_driver/java_test_driver.py
+++ src/java_test_driver/java_test_driver.py
@@ -1,3 +1,9 @@
+from utils.utils import Utils
+
+
 class JavaTestDriver:
-    def __init__(self):
-        pass
+    def __init__(self, test_driver_path: str) -> None:
+        self.test_driver_path = test_driver_path
+
+    def get_package_name(self) -> str:
+        return Utils.get_java_package_from_path(self.test_driver_path)
