4374008a1074b827d601e091ae26cb3e55b61cdb
eabalestra <agusbalestra0@gmail.com>
2025-08-07 15:02:30 -0300

    Rename assertions_file argument to buckets_assertions_file in testgen command

--- src/argsparser/parser.py
+++ src/argsparser/parser.py
@@ -21,7 +21,7 @@ def build_parser() -> argparse.ArgumentParser:
     testgen.add_argument("target_class_src")
     testgen.add_argument("test_suite")
     testgen.add_argument("test_driver")
-    testgen.add_argument("assertions_file")
+    testgen.add_argument("buckets_assertions_file")
     testgen.add_argument("method")
     testgen.add_argument(
         "-m",
