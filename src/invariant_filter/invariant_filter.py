37b2ce503e904ccc652d71f58ca90a1d7b6cd1b6
eabalestra <agusbalestra0@gmail.com>
2025-08-08 09:12:14 -0300

    Add InvariantFilter class skeleton for future implementation of invariant filtering functionality

---
+++ src/invariant_filter/invariant_filter.py
@@ -0,0 +1,4 @@
+class InvariantFilter:
+    def __init__(self):
+        # The idea is to implement the functionality of the two scripts using this class
+        raise NotImplementedError("Invariant filtering is not implemented yet.")
