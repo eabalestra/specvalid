c51a3bae6456b4ebcc2b6f72f68790219410a13e
eabalestra <agusbalestra0@gmail.com>
2025-08-12 10:01:09 -0300

    Add support for General_V2 prompt in PromptTemplateFactory

--- src/prompt/template_factory.py
+++ src/prompt/template_factory.py
@@ -2,6 +2,7 @@ from prompt.prompt_template import Prompt, PromptID
 
 from prompt.templates.compilation_fix.compilation_fix_prompt import CompilationFixPrompt
 from prompt.templates.test_generation.general.general_prompt import GeneralPrompt
+from prompt.templates.test_generation.zero_shot.zero_shot_prompt import ZeroShotPrompt
 
 
 class PromptTemplateFactory:
@@ -9,6 +10,8 @@ class PromptTemplateFactory:
     def create_prompt(prompt_id, class_code, method_code, spec) -> Prompt:
         if prompt_id == PromptID.General_V1:
             return GeneralPrompt(PromptID.General_V1, class_code, method_code, spec)
+        if prompt_id == PromptID.General_V2:
+            return ZeroShotPrompt(PromptID.General_V2, class_code, method_code, spec)
         if prompt_id == PromptID.NOT_COMPILABLE:
             return CompilationFixPrompt(
                 PromptID.NOT_COMPILABLE, class_code, method_code, spec
